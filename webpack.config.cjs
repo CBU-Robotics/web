5f270949f2907838224ec084f82af12ee80ae14d
eliasmurcray <eliasmurcray@gmail.com>
2024-04-28 08:43:51 -0700

    Update webpack config

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -5,62 +5,66 @@ const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const fs = require("node:fs");
-const basenames = JSON.parse(
-  fs.readFileSync("./templateconfig.json")
-).basenames;
+const basenames = JSON.parse(fs.readFileSync("./templateconfig.json")).basenames;
 
 const entries = {};
 const htmlPlugins = [];
 
 basenames.forEach((name) => {
-  entries[name] = `./src/chunks/${name}.tsx`;
-  htmlPlugins.push(
-    new HtmlWebpackPlugin({
-      template: `./src/html/${name}.html`,
-      filename: name + ".html",
-      chunks: [name],
-    })
-  );
+	entries[name] = `./src/chunks/${name}.tsx`;
+	htmlPlugins.push(
+		new HtmlWebpackPlugin({
+			template: `./src/html/${name}.html`,
+			filename: name + ".html",
+			chunks: [name],
+		}),
+	);
 });
 
 module.exports = {
-  entry: entries,
-  output: {
-    publicPath: "",
-    path: path.resolve(__dirname, "public"),
-    filename: "[name].[contenthash].js",
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx"],
-  },
-  optimization: {
-    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: "./src/assets/",
-        },
-      ],
-    }),
-    ...htmlPlugins,
-  ],
+	mode: "production",
+	entry: entries,
+	output: {
+		publicPath: "",
+		path: path.resolve(__dirname, "public"),
+		filename: "[name].[contenthash].js",
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
+			},
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: [".js", ".jsx", ".ts", ".tsx"],
+	},
+	optimization: {
+		minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "[name].[contenthash].css",
+		}),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: "./src/assets/",
+				},
+			],
+		}),
+		...htmlPlugins,
+	],
+	performance: {
+		hints: false,
+		maxEntrypointSize: 512000,
+		maxAssetSize: 512000,
+	},
 };
